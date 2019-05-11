bf7d78ccb759d261268e40061eb8351253e2ab51
Martín Molina Álvarez <tm07081995@gmail.com>
2019-05-11 22:39:56 +0200

    Fix default screen

--- src/screens/Container.js
+++ src/screens/Container.js
@@ -7,7 +7,7 @@ import React from 'react'
 const MainNavigator = createDrawerNavigator(
     screens,
     {
-        initialRouteName: 'Prizes',
+        initialRouteName: 'Home',
         //headerMode: 'none',
         contentComponent: props => <SideBar {...props} />
     }
